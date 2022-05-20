97e002a621a62fea6b7921e400acd5aeb6057ae5
JamesCalebWay <jcway324@gmail.com>
2022-05-19 21:43:37 -0400

    Finished Project

--- function.js
+++ function.js
@@ -1,22 +1,38 @@
 const names = ["Hands", "Stone Pick", "Torch", "Stone Hatchet", "Spear",
     "Wooden Club", "Slingshot", "Bow-Stone", "Bow-Tranq", "Metal Pick", 
-    "Metal Hatchet", "Pike", "Crossbow-Stone", "Crossbow-Tranq", "Simple Pistal",
-    "Sickle", "Metal Sword", "Longneck-Simple", "Longneck-Tranq", "Longneck-Shock",
-    "Shotgun", "Fab. Pistal", "Pump-Action", "Assault Rifle", "Compound Bow-Stone",
-    "Compound Bow-Tranq", "Compound Bow-Metal", "Fab. Sniper Rifle",
-    "Electric Prod", "Ranged Melee", "Tek Gauntlets", "Tek Sword",
-    "Tek Sword-Tek Gauntlets", "Tek Rifle", "Tek Railgun"];
+    "Metal Hatchet", "Pike", "Crossbow-Stone", "Crossbow-Tranq", "Simple Pistol",
+    "Sickle", "Metal Sword", "Longneck-Simple", "Longneck-Tranq Dart", "Longneck-Shocking",
+    "Shotgun", "Fabricated Pistol", "Pump-Action", "Assault Rifle", "Compound Bow-Stone",
+    "Compound Bow-Tranq", "Compound Bow-Metal", "Fabricated Sniper Rifle",
+    "Electric Prod", "Any Ranged Melee Attack", "Tek Gauntlets", "Tek Sword",
+    "Tek Sword w Tek Gauntlets", "Tek Rifle", "Tek Railgun"];
 const damage = [8, 16, 15, 20, 30, 5, 14, 55, 20, 32, 40, 55, 95, 35, 63,
-    50, 90, 280, 26, 26, 64, 50, 46, 47, 74, 27, 202.5, 165, 1, 10, 265,
+    50, 90, 280, 26, 26, 640, 50, 644, 47, 74, 27, 202.5, 165, 1, 10, 265,
     200, 280, 240, 935]
 
 function gunDmg(index)
 {
     var mod = document.getElementById(names[index] + "mod").value;
-    mod = (damage[index] * (mod / 100));
-    mod = Math.round(((damage[index] * (mod / 100)) + Number.EPSILON) * 100) / 100;
+    var total = Math.round(((damage[index] * (mod / 100)) + Number.EPSILON) * 100) / 100;
 
-    return document.getElementById(names[index]).innerHTML = "Output: " + mod;
+    return document.getElementById(names[index]).innerHTML = "Total: " + total;
+}
+
+function pelletDmg(index)
+{
+    var mod = document.getElementById(names[index] + "mod").value;
+
+    if (names[index] == "Shotgun")
+    {
+        var pell = 10;
+    }
+    if (names[index] == "Pump-Action")
+    {
+        var pell = 14;
+    }
+    var total = Math.round((((damage[index] / pell) * (mod / 100)) + Number.EPSILON) * 100) / 100;
+
+    return document.getElementById(names[index] + "per").innerHTML = "Per Pellet: " + total;
 }
 
 function meleeDmg(index)
@@ -26,5 +42,5 @@ function meleeDmg(index)
 
     var total = Math.round(((damage[index] * (mod / 100) * (human / 100)) + Number.EPSILON) * 100) / 100;
 
-    return document.getElementById(names[index]).innerHTML = "Output: " + total;
+    return document.getElementById(names[index]).innerHTML = "Total: " + total;
 }
