9c56e38cf86f7436307fa8894568b5f87644e76a
JamesCalebWay <jcway324@gmail.com>
2022-05-26 17:30:09 -0400

    Added new weapons, torpor calculations, and mobile images

--- function.js
+++ function.js
@@ -1,14 +1,15 @@
 const names = ["Hands", "Stone Pick", "Torch", "Stone Hatchet", "Spear",
-    "Wooden Club", "Slingshot", "Bow-Stone", "Bow-Tranq", "Metal Pick", 
-    "Metal Hatchet", "Pike", "Crossbow-Stone", "Crossbow-Tranq", "Simple Pistol",
-    "Sickle", "Metal Sword", "Longneck-Simple", "Longneck-Tranq Dart", "Longneck-Shocking",
-    "Shotgun", "Fabricated Pistol", "Pump-Action", "Assault Rifle", "Compound Bow-Stone",
-    "Compound Bow-Tranq", "Compound Bow-Metal", "Fabricated Sniper Rifle",
-    "Electric Prod", "Any Ranged Melee Attack", "Tek Gauntlets", "Tek Sword",
-    "Tek Sword w Tek Gauntlets", "Tek Rifle", "Tek Railgun"];
+    "Wooden Club", "Slingshot", "Bow-Stone Arrow", "Bow-Tranq Arrow", "Metal Pick", 
+    "Metal Hatchet", "Pike", "Crossbow-Stone Arrow", "Crossbow-Tranq Arrow", "Simple Pistol",
+    "Sickle", "Metal Sword", "Longneck-Simple Rifle Ammo", "Longneck-Tranq Dart", "Longneck-Shocking Tranq Dart",
+    "Shotgun", "Fabricated Pistol", "Pump-Action Shotgun", "Assault Rifle", "Compound Bow-Stone Arrow",
+    "Compound Bow-Tranq Arrow", "Compound Bow-Metal Arrow", "Fabricated Sniper Rifle",
+    "Electric Prod", "Any Ranged Melee Attack", "Tek Gauntlets (Chrg)", "Tek Sword",
+    "Tek Sword w/ Tek Gauntlets", "Tek Rifle", "Tek Railgun", "Bow-Toxicant Arrow",
+    "Crossbow-Toxicant Arrow", "Compound Bow-Toxicant Arrow"];
 const damage = [8, 16, 15, 20, 30, 5, 14, 55, 20, 32, 40, 55, 95, 35, 63,
     50, 90, 280, 26, 26, 640, 50, 644, 47, 74, 27, 202.5, 165, 1, 10, 265,
-    200, 280, 240, 935]
+    200, 280, 240, 935, 20, 35, 27]
 
 function gunDmg(index)
 {
@@ -26,7 +27,7 @@ function pelletDmg(index)
     {
         var pell = 10;
     }
-    if (names[index] == "Pump-Action")
+    if (names[index] == "Pump-Action Shotgun")
     {
         var pell = 14;
     }
@@ -43,4 +44,23 @@ function meleeDmg(index)
     var total = Math.round(((damage[index] * (mod / 100) * (human / 100)) + Number.EPSILON) * 100) / 100;
 
     return document.getElementById(names[index]).innerHTML = "Total: " + total;
+}
+
+function torpor(index, torpMod)
+{
+    var mod = document.getElementById(names[index] + "mod").value;
+    var human = document.getElementById(names[index] + "hum").value;
+
+    var torpor = Math.round(((damage[index] * (mod / 100) * (human / 100) * torpMod) + Number.EPSILON) * 100) / 100;
+
+    return document.getElementById(names[index] + "torp").innerHTML = "Torpor: " + torpor;
+}
+
+function gunTorpor(index, torpMod)
+{
+    var mod = document.getElementById(names[index] + "mod").value;
+
+    var torpor = Math.round(((damage[index] * (mod / 100) * torpMod) + Number.EPSILON) * 100) / 100;
+
+    return document.getElementById(names[index] + "torp").innerHTML = "Torpor: " + torpor;
 }
